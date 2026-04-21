1397a0d963c84675d2af933eba3a78049cd97e61
NDJINGA Michael 206165 <mn206165@is246973.intra.cea.fr>
2026-04-22 01:24:23 +0200

    Updated comments

--- tests/src/testSaddlePointLinearSolver_2x2_API_Schur.c
+++ tests/src/testSaddlePointLinearSolver_2x2_API_Schur.c
@@ -1,41 +1,14 @@
 static char help[] = "Read a PETSc matrix from a file -f0 <input file>\n Parameters : \n -f0 : matrix fileName \n -nU :number of velocity lines \n -nP : number of pressure lines \n -mat_type : PETSc matrix type \n ";
 
 /*************************************************************************************************/
-/* Parallel implementation of a transform-then-solve preconditioner for the linear system A_{input} X_{output} = b_{input} */
-/*            Use an upper block triangular matrix U and perform the change of variables  X_hat = U^{-1}X, A_hat = A_{input}*U */
-/*            The transformed matrix Ahat is close to a lower triangular matrix Pmat used as preconditioner */
+/* Parallel implementation of a Schur type preconditioner for the linear system A_{input} X_{output} = b_{input} */
 /*                                                                                               */
-/* Description : Parallel file with PC_COMPOSITE of MULTIPLICATIVE type, not restricted to 2x2 blocs.*/
+/* Description : Parallel file with PC_FIELDSPLIT_SCHUR_PRE type, restricted to 2x2 blocs.*/
 /*               Use of API (class SaddlePointLinearSolve) for better code factorisation         */ 
-/*               The inverse of M is approximated Using ILU factorisation                        */ 
 /*                                                                                               */
 /* Input  : - Matrix A_{input}    (system matrix, loaded from a file)                            */
 /*          - Vector b_{input}    (right hand side, made up for testing)                         */
 /* Output : - Vector X_{output}   (unknown vector, to be determined)                             */
-/*                                                                                               */
-/* Auxilliary variables : - A_hat (transformed matrix)                                           */
-/*                        - X_hat (unknown of the transformed system)                            */
-/*                        - Pmat  (preconditioning matrix)                                       */
-/*                        - M top    left  submatrix of A_{input}                                */
-/*                        - G top    right submatrix of A_{input}                                */
-/*                        - D bottom left  submatrix of A_{input}                                */
-/*                        - C bottom right submatrix of A_{input}                                */
-/*                                                                                               */
-/*                                 *M   G*                                                       */
-/*                        A     = *       *                                                      */
-/*                                 *D   C*                                                       */
-/*                                                                                               */
-/*                                 *Id  -diag(M)^{-1}G*                    *Id  tM^{-1}G*   */
-/*                        U     = *                    *         U^{-1} = *                   *  */
-/*                                 *0               Id*                    *0              Id*   */
-/*                                                                                               */
-/*                                 *M     G_hat*             G_hat=G - M*tM^{-1}*G          */
-/*                        A_hat = *             *                                                */
-/*                                 *D     C_hat*             C_hat=C - D*dtM^{-1}*G          */
-/*                                                                                               */
-/*                                 *2 diag(M)     0  *                                           */
-/*                        Pmat  = *                   *                                          */
-/*                                 *D          C_hat *                                           */
 /*                                                                                               */
 /*************************************************************************************************/
 
@@ -43,14 +16,13 @@ static char help[] = "Read a PETSc matrix from a file -f0 <input file>\n Paramet
 
 int main( int argc, char **args ){
 	PetscInitialize(&argc,&args, (char*)0,help);
-	PetscInt n_u, n_p, n;//Total number of velocity and pressure lines. n = n_u+ n_p
+	PetscInt n_u, n_p;//Total number of velocity and pressure lines. n = n_u+ n_p
 	char file[1][PETSC_MAX_PATH_LEN], mat_type[256]; // File to load, matrix type
 	Mat A_input;
 	Mat M, G, D, C;
 	IS is_U,is_P;
 	Vec b_input, X_anal, X_output, X_u, X_p;
 	double error,  rtol=1e-7, residu;
-    PC pcshell;
 
     /* Minimum profiling for cpu time */
 	PetscLogStage  linear_system_stage;
@@ -63,25 +35,23 @@ int main( int argc, char **args ){
 	PetscOptionsGetString(NULL,NULL,"-mat_type",mat_type,sizeof(mat_type),NULL);
 	PetscOptionsGetInt(NULL,NULL,"-nU",&n_u,NULL);
 	PetscOptionsGetInt(NULL,NULL,"-nP",&n_p,NULL);
-	n=n_u+n_p;
 
 	loadPETScMat( file[0], mat_type, &A_input, n_u, n_p);
-	
 	splitPETScMatrix2x2(   A_input, n_u, n_p, &M, &G, &D, &C, &is_U, &is_P);
-
 	buildRHSVector( A_input, n_u, n_p, &X_anal, &b_input);
 
-	VecDuplicate(b_input,&X_output);// X_output will store the numerical solution of the linear system
 //##### Calling KSP solver and monitor convergence
 	PetscLogStageRegister("Résolution du système linéaire", &linear_system_stage);//Instrumentation : début de la résolution du second membre
 	PetscLogStagePush( linear_system_stage);//Instrumentation
 
+	VecDuplicate(b_input,&X_output);// X_output will store the numerical solution of the linear system
     solveSchurSystemForXoutput( A_input, is_U, is_P, b_input, &X_output, rtol,PETSC_DEFAULT,PETSC_DEFAULT, PETSC_DEFAULT, &residu);
 
 	PetscLogStagePop();//Instrumentation : fin de la résolution du second membre
 	PetscLogStageGetPerfInfo( linear_system_stage, &info_linear_system_stage);
 	PetscPrintf(PETSC_COMM_WORLD, "\nTime taken to solve the linear system : %e \n\n",info_linear_system_stage.time);
 	
+//##### Check the solution is correct
 	PetscCall( VecGetSubVector( X_output, is_P, &X_p) );
 	PetscCall( VecGetSubVector( X_output, is_U, &X_u) );
 
