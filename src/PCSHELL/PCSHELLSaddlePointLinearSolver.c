b5c8b64fddcd8330b5bfa775a409094cad6c1ef9
NDJINGA Michael 206165 <mn206165@is246973.intra.cea.fr>
2026-04-22 00:55:13 +0200

    Compiled the PCSHELL

--- src/PCSHELL/PCSHELLSaddlePointLinearSolver.c
+++ src/PCSHELL/PCSHELLSaddlePointLinearSolver.c
@@ -14,20 +14,24 @@
 PetscErrorCode setup(PC pcshell)
 {
     SaddlePointCtx2x2 * ctx;
+    Mat          Amat, Pmat;     /* The system matrix */
     PC               subpc1;     /* The PCFIELDSPLIT sub-preconditioner1 */
     PC               subpc2;     /* The PCFIELDSPLIT sub-preconditioner2 */
     PetscInt nrows, ncolumns;//Total number of rows and columns of A_input
     PetscInt irow_min, irow_max;//min and max indices of rows stored locally on this process
 	PetscInt n_u, n_p;//Total number of velocity and pressure lines. n = n_u+ n_p
-
+    KSP *kspArray;
+    IS is_U, is_P;
 
     PetscFunctionBegin;
 
 //#### Building the index sets is_U and is_P
     PetscCall(PCShellGetContext( pcshell, &ctx));
     n_u = ctx->n_u;
-    MatGetOwnershipRange( ctx->A_input, &irow_min, &irow_max);
-    MatGetSize( ctx->A_input, &nrows, &ncolumns);
+    PCGetOperators( pcshell, &Amat, &Pmat);
+    MatGetOwnershipRange( Pmat, &irow_min, &irow_max);
+    MatGetSize( Pmat, &nrows, &ncolumns);
+    
     PetscInt min_pressure_lines = irow_min <= n_u ? n_u : irow_min;//max(irow_min, n_u)
     PetscInt max_velocity_lines = irow_max >= n_u ? n_u : irow_max;//min(irow_max, n_u)
     PetscInt nb_pressure_lines = irow_max >= n_u ? irow_max - min_pressure_lines : 0;
@@ -35,11 +39,36 @@ PetscErrorCode setup(PC pcshell)
     PetscInt nb_local_lines = irow_max - irow_min; 
 
     PetscCheck(   nrows == ncolumns, PETSC_COMM_WORLD, PETSC_ERR_ARG_SIZ, "Matrix is not square !!!\n");
-    PetscCheck( n_u+n_p == ncolumns, PETSC_COMM_WORLD, PETSC_ERR_ARG_SIZ, "Inconsistent data : the matrix has %d lines but only %d velocity lines and %d pressure lines declared\n", ncolumns, n_u,n_p);
+    PetscCheck( n_u+n_p == ncolumns, PETSC_COMM_WORLD, PETSC_ERR_ARG_SIZ, "Inconsistent data : the matrix has %d lines but only %d velocity lines and %d pressure lines declared.\n Only velocity and pressure unknowns handled now\n", ncolumns, n_u,n_p);
     
-    ISCreateStride(PETSC_COMM_WORLD, nb_velocity_lines, max_velocity_lines - nb_velocity_lines, 1, is_U);
-    ISCreateStride(PETSC_COMM_WORLD, nb_pressure_lines, min_pressure_lines                    , 1, is_P);
+    ISCreateStride(PETSC_COMM_WORLD, nb_velocity_lines, max_velocity_lines - nb_velocity_lines, 1, &is_U);
+    ISCreateStride(PETSC_COMM_WORLD, nb_pressure_lines, min_pressure_lines                    , 1, &is_P);
 
+//#### The PCFIELDSPLIT preconditioner (based on GAMG and ILU) ###//
+    PCCreate(PETSC_COMM_WORLD, &ctx->pc);
+    PCSetType( ctx->pc, PCFIELDSPLIT);
+    PCFieldSplitSetType( ctx->pc, PC_COMPOSITE_SCHUR);
+
+    PCFieldSplitSetIS( ctx->pc, "velocity",is_U);//The order here matters a lot between this line and the next
+    PCFieldSplitSetIS( ctx->pc, "pressure",is_P);//The order here matters a lot between this line and the previous
+    PCFieldSplitSetSchurPre(      ctx->pc, PC_FIELDSPLIT_SCHUR_PRE_SELFP,NULL);//or PC_FIELDSPLIT_SCHUR_PRE_USER if you provide Chat
+    PCFieldSplitSetSchurFactType( ctx->pc, PC_FIELDSPLIT_SCHUR_FACT_FULL);
+    PetscCall( PCSetUp( ctx->pc) );
+    PCFieldSplitSchurGetSubKSP( ctx->pc, NULL, &kspArray);
+    KSPSetType( kspArray[0], KSPPREONLY);
+    KSPSetType( kspArray[1], KSPPREONLY);
+    KSPGetPC(kspArray[0], &subpc1);
+    KSPGetPC(kspArray[1], &subpc2);
+
+    PCSetType( subpc1, PCJACOBI);
+    PCSetType( subpc2, PCBJACOBI);
+
+    PCDestroy(&subpc1);
+    PCDestroy(&subpc2);
+    ISDestroy(&is_U);
+    ISDestroy(&is_P);
+    PetscFree(kspArray);
+    
     PetscFunctionReturn(PETSC_SUCCESS);
 }
 
@@ -50,12 +79,7 @@ PetscErrorCode destroy(PC pcshell)
     
     PetscFunctionBegin;
     PetscCall(PCShellGetContext( pcshell, &ctx));
-    VecDestroy(&ctx->GX_p);
-    VecDestroy(&ctx->Y_u);
-    VecDestroy(&ctx->Y_p);
-    VecDestroy(&ctx->X_u);
-    VecDestroy(&ctx->X_p);
-    PCDestroy(&ctx->pcM);
+    PCDestroy(&ctx->pc);
     PetscFunctionReturn(PETSC_SUCCESS);
 }
 
@@ -66,18 +90,8 @@ PetscErrorCode apply(PC pcshell, Vec x, Vec y)
     
     PetscFunctionBegin;
     PetscCall(PCShellGetContext( pcshell, &ctx));
-    PetscCall(VecGetSubVector( x, ctx->is_P, &ctx->X_p) );
-    PetscCall(VecGetSubVector( x, ctx->is_U, &ctx->X_u) );
-    PetscCall(VecGetSubVector( y, ctx->is_P, &ctx->Y_p) );
-    PetscCall(VecGetSubVector( y, ctx->is_U, &ctx->Y_u) );
 
-    PetscCall(MatMult( ctx->G,   ctx->X_p,  ctx->GX_p) );//GX_p contains G*x_p
-    PetscCall(PCApply( ctx->pcM, ctx->GX_p, ctx->Y_u ) );//Y_u contains M^{-1}*G*x_p
-    PetscCall(VecAYPX( ctx->Y_u, -1, ctx->X_u) );//X_u contains X_u - M^{-1}*G*x_p
-    VecCopy(ctx->X_p,ctx->Y_p);
+    PetscCall(PCApply( ctx->pc, x, y ) );
 
-    PetscCall(VecRestoreSubVector( x, ctx->is_P, &ctx->X_p) );
-    PetscCall(VecRestoreSubVector( x, ctx->is_U, &ctx->X_u) );
     PetscFunctionReturn(PETSC_SUCCESS);
 }
-
--- src/PCSHELL/PCSHELLSaddlePointLinearSolver.h
+++ src/PCSHELL/PCSHELLSaddlePointLinearSolver.h
@@ -3,12 +3,15 @@
 
 /*
      User-defined preconditioner context
+     For the moment only velocity and pressure unknowns handled (2x2 system)
+     - the velocity indices are supposed to be contiguous
+     - the pressure indices are supposed to be contiguous
+     - the velocity indices come before the pressure indices in the unknown vector
 */
 typedef struct {
-   PC           pc;       /* The PCFIELDSPLIT main preconditioner (output)*/
-   Mat     A_input;       /* The system matrix */
-   PetscMPIInt n_U;       /* number of velocity lines (input)*/
-   PetscMPIInt n_P;       /* number of pressure lines (input)*/
+   PetscInt n_u;       /* number of velocity lines (input) */
+   PetscInt n_p;       /* number of pressure lines (input) */
+   PC        pc;       /* The PCFIELDSPLIT preconditioner suited for saddle point matrices (output) */
  } SaddlePointCtx2x2;
 
 
